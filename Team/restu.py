9cf31daae3bc552848c463fa09d6778a225fc22a
Rezzttt <169017654+Rezzttt@users.noreply.github.com>
2025-02-19 20:00:17 +0700

    Add files via upload

---
+++ Team/restu.py
@@ -0,0 +1,9 @@
+def perkenalan(nama, nim, role):
+    print("Hai, Perkenalan, Nama Saya", nama)
+    print("NIM : ", nim)
+    print("Role saya pada Projek ini yaitu : ", role)
+if __name__ == "__main__":
+    nama = "Restu Narendrastomo"
+    nim = "22.130.10.365"
+    role = "Presenter"
+    perkenalan(nama, nim, role)
