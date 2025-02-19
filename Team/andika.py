b9c414fa85989f78789852a33d0b7ac5274f8887
PRDN <yudiputro7@gmail.com>
2025-02-19 19:43:00 +0700

    Add files via upload

---
+++ Team/andika.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, role):
+    print("Hai, Perkenalan, Nama Saya", nama)
+    print("NIM : ", nim)
+    print("Role saya pada Projek ini yaitu : ", role)
+
+if __name__ == "__main__":
+    nama = "Andika Ilham Pradana"
+    nim = "22.130.10.352"
+    role = "Programmer"
+    perkenalan(nama, nim, role)
