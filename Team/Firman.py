0afdfa807abd85230bab04234e8b7ac4acaaf29e
Firmanandri1904 <andriantofirman2@gmail.com>
2025-02-19 20:44:44 +0700

    Add files via upload

---
+++ Team/Firman.py
@@ -0,0 +1,9 @@
+def perkenalan(nama, nim, role):
+    print("Hai, Perkenalan, Nama Saya", nama)
+    print("NIM : ", nim)
+    print("Role saya pada Projek ini yaitu : ", role)
+if __name__ == "__main__":
+    nama = "Firman Andrianto Arief"
+    nim = "22.130.10.357"
+    role = "Desainer"
+    perkenalan(nama, nim, role)
